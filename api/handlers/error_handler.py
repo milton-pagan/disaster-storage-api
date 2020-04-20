60f019c03ef5a8651568fc9fec30fafa4316e686
Milton Pagan <milton.pagan1@upr.edu>
2020-04-20 19:56:30 -0400

    Change api module to app

--- api/handlers/error_handler.py
+++
@@ -1,9 +0,0 @@
-from flask import make_response, jsonify
-
-
-class ErrorHandler(object):
-    def not_found(self):
-        return make_response(jsonify({"Error": "Not found"}), 404)
-
-    def bad_request(self, message="Bad request"):
-        return make_response(jsonify({"Error": message}), 400)
