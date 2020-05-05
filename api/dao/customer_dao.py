af497ac3defe18101e38f99627b12b9bd9a86215
Jesus Garcia Marrero <jesus.garcia13@upr.edu>
2020-05-05 00:29:15 -0400

    Added querys to CustomerDAO and Fixed getAll and getById in CustomerHandler. Added more get methods. Added root in app.py.

--- api/dao/customer_dao.py
+++ api/dao/customer_dao.py
@@ -1,5 +1,6 @@
 import psycopg2
-
+import psycopg2.extras
+from api.config.config import get_config
 
 class CustomerDAO(object):
     def __init__(self):
@@ -8,19 +9,58 @@ class CustomerDAO(object):
     # General Customer Operations
 
     def get_all_customer(self):
-        return [
-            (1, "Dionel", "Martinez", "Santa Isabel", 3),
-            (2, "Milto", "Pagan", "Juana Diaz", 7),
-            (3, "Jesus", "Garcia", "San Juan", 8),
-        ]
+        cursor = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        query = "SELECT user_id, username, phone, customer_id, customer_first_name, customer_last_name, customer_city, location_id FROM customer NATURAL INNER JOIN PUBLIC.USER ORDER BY customer_id;"
+        cursor.excecute(query)
+
+        return cursor.fetchall()
 
     def get_customers_by_city(self, customer_city):
-        return [(1, "Dionel", "Martinez", "Santa Isabel", 3)]
+        cursor = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        query = "SELECT customer_id, customer_name FROM customer WHERE customer_city=%s ORDER BY customer_id"
+        cursor.excecute(query, (customer_city,))
+
+        return cursor.fetchall()
 
     # Operations using Customer Id
 
     def get_customer_by_id(self, customer_id):
-        return [(1, "Dionel", "Martinez", "Santa Isabel", 3)]
+        cursor = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        query = "SELECT user_id, username, phone, customer_id, customer_first_name, customer_last_name, customer_city, location_id FROM customer NATURAL INNER JOIN PUBLIC.USER WHERE customer_id=%s ORDER BY customer_id"
+        cursor.excecute(query, (customer_id,))
+
+        return cursor.fetchone()
+
+    def get_customer_location_by_id(self, customer_id):
+        cursor = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        query = "SELECT latitude, longitude FROM location NATURAL INNER JOIN customer WHERE customer_id=%s;"
+        cursor.excecute(query, (customer_id,))
+
+        return cursor.fetchall()
+
+    # Operations that return the products names
+    # that (order, reserve or request) a specific customer
+
+    def get_product_ordered_by_customer(self, customer_id):
+        cursor = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        query = "SELECT product_name FROM orders NATURAL INNER JOIN buys NATURAL INNER JOIN product WHERE customer_id=%s;"
+        cursor.excecute(query, (customer_id,))
+
+        return cursor.fetchall()
+
+    def get_product_reserved_by_customer(self, customer_id):
+        cursor = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        query = "SELECT product_name FROM orders NATURAL INNER JOIN reserves NATURAL INNER JOIN product WHERE customer_id=%s;"
+        cursor.excecute(query, (customer_id,))
+
+        return cursor.fetchall()
+
+    def get_product_requested_by_customer(self, customer_id):
+        cursor = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        query = "SELECT product_name FROM orders NATURAL INNER JOIN request NATURAL INNER JOIN product WHERE customer_id=%s;"
+        cursor.excecute(query, (customer_id,))
+
+        return cursor.fetchall()
 
     def insert_customer(
         self,
